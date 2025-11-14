a3dc03b4cc44a6eeeb777335fedf483807409dde
Victor <victorticu2003@gmail.com>
2025-11-14 14:44:12 -0600

    se termino el endpoint para crear un pedido

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const rutasUsuario = require('./routes/rutasUsuarios.js');
 const rutasResenas = require('./routes/rutasResenas.js');
 const rutasWishlist = require('./routes/rutasWishlist.js');
+const rutasPedidos = require('./routes/rutasPedidos.js')
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
@@ -19,14 +20,13 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json()); 
 
-// para usar las rutas de usuario.js
 app.use('/usuarios', rutasUsuario);
 
-// para usar las rutas de resenas.js
 app.use('/resenas', rutasResenas);
 
-// para usar las rutas de wishlist.js
-app.use('/wishlist', rutasWishlist);
+app.use('/pedidos', rutasPedidos)
+
+app.use('/wishlist', rutasWishlist); 
 
 app.get('/', (req, res) => {
   res.json({ message: 'API funcionando correctamente' });
